10f617a7f70e58ff348058351dcf637a10c9d2a4
DavidCZ2051 <84991177+DavidCZ2051@users.noreply.github.com>
2022-11-01 20:35:09 +0100

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,3 +1,6 @@
+// packages
+import 'package:flutter/material.dart';
+
 const String url = "localhost";
 const String appVersion = "1.0.0-DEV";
 const bool debug = true; //debug variable
@@ -19,4 +22,20 @@ class User {
   String? firstName;
   String? lastName;
   String? token;
+  User({this.firstName, this.lastName, this.token});
+}
+
+class Competition {
+  int id;
+  DateTime startDate;
+  DateTime endDate;
+  String type;
+  String description;
+  Competition({
+    required this.id,
+    required this.startDate,
+    required this.endDate,
+    required this.type,
+    required this.description,
+  });
 }
