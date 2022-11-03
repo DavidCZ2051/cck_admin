7d2d0a0a27da5efc5b02fa48c1a3456a7f170093
DavidCZ2051 <84991177+DavidCZ2051@users.noreply.github.com>
2022-11-03 08:43:36 +0100

    wed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 //packages
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'package:window_size/window_size.dart';
 //routes
 import 'package:cck_admin/routes/lobby.dart';
 import 'package:cck_admin/routes/login.dart';
@@ -9,13 +7,6 @@ import 'package:cck_admin/routes/settings.dart';
 import 'package:cck_admin/routes/competitions.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    setWindowTitle('ČČK Admin');
-    setWindowVisibility(visible: true);
-    setWindowMaxSize(const Size(1920, 1080));
-    setWindowMinSize(const Size(500, 700));
-  }
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
