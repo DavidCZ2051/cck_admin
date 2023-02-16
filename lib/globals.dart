6b5bfd374424dfa8800e7105c92d305a5fa0e7d5
DavidCZ2051 <84991177+DavidCZ2051@users.noreply.github.com>
2023-02-16 20:01:35 +0100

    Revert "Update globals.dart"
    This reverts commit 9ba244b359adfd81f56caf4f0f845aa7cf0e001a.

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,7 +1,7 @@
 // packages
 import 'package:flutter/material.dart';
 
-const String url = "https://localhost:5000"; //"https://localhost:7041";
+const String url = "https://localhost:7041";
 const String appVersion = "1.0.0-BETA";
 const bool debug = false; //debug variable
 
